408272b10244dde3c653fb75cf2f150b159877bd
Rafael Joaquim <rafaelcybersistemas@gmail.com>
2020-12-29 18:26:58 -0300

    subindo o projeto criado através do https://start.spring.io/

---
+++ src/test/java/com/accenture/documentsApi/DocumentsApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.accenture.documentsApi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DocumentsApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
